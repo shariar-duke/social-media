d59090fb8a333bc8cbf709ed01f3df70b9d6a760
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-29 20:12:38 +0600

    all pages created intiially

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
   return (
