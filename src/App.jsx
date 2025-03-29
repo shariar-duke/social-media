d5a1d99df8de22442e920fa2e7522a65ba5df6de
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-29 15:58:30 +0600

    confguration

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="page-container">
+      <div className="border border-black">
+        <h1 className="text-3xl font-bold underline text-lwsGreen">
+          Hello world!
+        </h1>
+      </div>
+    </div>
   );
 }
 
