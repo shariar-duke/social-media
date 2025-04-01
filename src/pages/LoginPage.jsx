73258a03acaa0fa088f76687cef50a65c75970a5
Shariar Duke <shariarmahmud225@gmail.com>
2025-04-01 06:57:41 +0600

    images added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import AuthIllustration from "../../src/assets/images/auth_illustration.png";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-deepDark py-8">
+      <div className="max-w-[1368px] flex-1">
+        <div className="container grid items-center gap-8 lg:grid-cols-2">
+          <img
+            className="mb-12 max-w-full max-lg:hidden"
+            src={AuthIllustration}
+            alt="auth_illustration"
+          />
+        </div>
+        <h1 className="mb-3 text-4xl font-bold lg:text-[40px]">Facehook</h1>
+        <p className="max-w-[452px] text-gray-600/95 lg:text-lg">
+          Create a social media app with features like, showing the post, post
+          details, reactions, comments and profile.
+        </p>
+      </div>
+    </main>
+  );
 }
