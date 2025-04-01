d7d79c11fe30ab17d9208a98e6ebf94b23f75eae
Shariar Duke <shariarmahmud225@gmail.com>
2025-04-01 13:21:58 +0600

    protected route implementation completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProfilePage from "./pages/ProfilePage";
 import RegistrationPage from "./pages/RegistrationPage";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<HomePage />} path="/" exact />
+        <Route element={<PrivateRoute />}>
+          <Route element={<HomePage />} path="/" exact />
+          <Route element={<ProfilePage />} path="/me" />
+        </Route>
+
         <Route element={<LoginPage />} path="/login" />
         <Route element={<RegistrationPage />} path="/register" />
-        <Route element={<ProfilePage />} path="/me" />
+
         <Route element={<NotFoundPage />} path="*" />
       </Routes>
     </>
