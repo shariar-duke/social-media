74502fde48040a9060cf153c74f15ac142c2e5a1
Shariar Duke <shariarmahmud225@gmail.com>
2025-04-02 13:38:55 +0600

    profile page data fetched

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,36 @@
-import React from "react";
-
+import { useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { api } = useAxios();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchProfile = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user.id}`
+        );
+        setUser(response?.data.user);
+        setPosts(response?.data.posts);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+  if (loading) {
+    return <p>Loading your user data </p>;
+  }
+  return <div>{user?.firstName}</div>;
 }
