1b67b7ac9175447c761c458014c2740e86df88bb
Shariar Duke <shariarmahmud225@gmail.com>
2025-04-02 13:17:02 +0600

    real name dispayed in the header section

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,8 +3,10 @@ import HomeIcon from "../../assets/icons/home.svg";
 import Notification from "../../assets/icons/notification.svg";
 import Avatar from "../../assets/images/avatars/avatar_1.png";
 import Logo from "../../assets/images/logo.svg";
+import { useAuth } from "../../hooks/useAuth";
 import Logout from "../auth/Logout";
 const Header = () => {
+  const { auth } = useAuth();
   return (
     <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
       <div
@@ -28,7 +30,9 @@ sm:flex-row"
           </button>
           <Logout />
           <Link to="/me" className="flex-center !ml-8 gap-3">
-            <span className="text-lg font-medium lg:text-xl">Sumit</span>
+            <span className="text-lg font-medium lg:text-xl">
+              {auth?.user?.firstName}
+            </span>
             <img
               className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px]"
               src={Avatar}
