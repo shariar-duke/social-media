752c965577292f3f8632f04409e4ca369cbb2573
Shariar Duke <shariarmahmud225@gmail.com>
2025-04-04 08:51:34 +0600

    wrapped with profile Provider

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,18 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Header from "../components/common/Header";
 import { useAuth } from "../hooks/useAuth";
+import ProfileProvider from "../providers/ProfileProvider";
 export default function PrivateRoute() {
   const { auth } = useAuth();
   return (
     <>
       {auth.user ? (
         <div>
-          <Header />
-          <main className="mx-auto max-w-[1020px] py-8">
-            <div className="container">
-              <Outlet />
-            </div>
-          </main>
+          <ProfileProvider>
+            <Header />
+            <main className="mx-auto max-w-[1020px] py-8">
+              <div className="container">
+                <Outlet />
+              </div>
+            </main>
+          </ProfileProvider>
         </div>
       ) : (
         <Navigate to="/login" />
